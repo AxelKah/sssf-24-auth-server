79a7d4ac408ee508c8740c4a1e38adbf2fa44817
AxelKah <axel.kahkonen@gmail.com>
2024-04-22 13:58:12 +0300

    first commit

---
+++ src/api/routes/userRoute.ts
@@ -0,0 +1,24 @@
+import express from 'express';
+import {
+  userDelete,
+  userGet,
+  userListGet,
+  userPost,
+  userPut,
+} from '../controllers/userController';
+import {authenticate} from '../../middlewares';
+
+const router = express.Router();
+
+router
+  .route('/')
+  .get(userListGet)
+  .post(userPost)
+  .put(authenticate, userPut)
+  .delete(authenticate, userDelete);
+
+
+
+router.route('/:id').get(userGet);
+
+export default router;
